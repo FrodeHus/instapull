12eaf7e8a77ee314c301bf870b505d136b8e375e
frodehus <frode.hus@outlook.com>
2020-10-05 08:27:43 +0200

    added help tekst and options

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="instapull",
-    version="0.1",
+    version="0.2",
     packages=setuptools.find_packages(),
     entry_points={"console_scripts": ["instapull=instapull.__main__:main"]},
     author="Frode Hus",
@@ -10,7 +10,7 @@ setuptools.setup(
     description="Simple tool that lets you dump the imagestream from a Instagram user",
     url="https://www.frodehus.com",
     python_requires=">=3.6",
-    install_requires=["requests"],
+    install_requires=["requests", "argparse"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
