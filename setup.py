403cc33568873db2965da338de3d3829c1769f8f
Frode Hus <frode.hus@outlook.com>
2020-10-10 22:26:47 +0200

    updated description

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setuptools.setup(
     entry_points={"console_scripts": ["instapull=instapull.__main__:main"]},
     author="Frode Hus",
     author_email="frode.hus@outlook.com",
-    description="Simple tool that lets you dump the imagestream from a Instagram user",
+    description="Simple tool that lets you dump Instagram posts by user or hashtag",
     url="https://www.frodehus.com",
     python_requires=">=3.6",
     install_requires=["requests", "argparse"],
